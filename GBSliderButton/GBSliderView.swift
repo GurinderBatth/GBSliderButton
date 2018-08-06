a51ef65c1e4d5a9071c41f1dd32111f224303f16
Apple <apple@Apples-iMac.local>
2018-08-06 23:30:01 +0530

    First Commit

---
+++ GBSliderButton/GBSliderView.swift
@@ -0,0 +1,21 @@
+//
+//  GBSliderView.swift
+//  GBSliderButton
+//
+//  Created by Apple on 06/08/18.
+//  Copyright © 2018 Batth. All rights reserved.
+//
+
+import UIKit
+
+class GBSliderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
